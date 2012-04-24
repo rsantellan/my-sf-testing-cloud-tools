963c75dc28e9a8e1f64c45aa2a342a8ec96851f9
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2012-04-24 16:34:01 +0000

    Se agrega el primer plugin myBasicPlugin. El mismo tiene los modulos de i18n y upload de archivos.
    Los js, css y imagenes no estan todavias, tendria que crear un helper para el mismo.

--- trunk/lib/form/doctrine/myBasicPlugin/myUploadedForm.class.php
+++ trunk/lib/form/doctrine/myBasicPlugin/myUploadedForm.class.php
@@ -6,9 +6,9 @@
  * @package    testing
  * @subpackage form
  * @author     Your name here
- * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class myUploadedForm extends BasemyUploadedForm
+class myUploadedForm extends PluginmyUploadedForm
 {
   public function configure()
   {
