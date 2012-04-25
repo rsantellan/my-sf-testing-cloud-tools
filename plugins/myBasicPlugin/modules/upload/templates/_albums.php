4761454705f0fa9d535fd2da3573dcc9eba319bc
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2012-04-25 20:04:04 +0000

    Se agrega el slider.

--- plugins/myBasicPlugin/modules/upload/templates/_albums.php
+++ plugins/myBasicPlugin/modules/upload/templates/_albums.php
@@ -4,10 +4,15 @@
   <a  class="fancy_link iframe" href="<?php echo url_for("upload/upload?i=".$album->getId()."&c=".$objectClass);?>">upload</a>
   <br/>
   <?php $uploades = $album->getMyUploaded(); ?>
-  
-  <div id="album_images_<?php echo $album->getId();?>">
-    <?php include_partial("upload/albumImages", array("uploades" => $uploades)); ?>
+  <div id="view_album_images_<?php echo $album->getId();?>">
+    <div id="album_images_<?php echo $album->getId();?>" class="album_images">
+      <div id="album_images_container_<?php echo $album->getId();?>" class="album_images_container">
+        <?php include_partial("upload/albumImages", array("uploades" => $uploades)); ?>
+      </div>
+    </div>
   </div>
+  <div id="album_slider_<?php echo $album->getId();?>"></div>
+  <div class="clear"></div>
 <?php endforeach;?>
 
 <input type="hidden" value="<?php echo url_for("@reloadAlbum")?>" id="place_to_reload_albums" />
--- plugins/myBasicPlugin/web/js/uploader.js
+++ plugins/myBasicPlugin/web/js/uploader.js
@@ -1,6 +1,8 @@
 $(document).ready(function() { 
   
   $("a.fancy_link").fancybox();
+  initializaseAllAlbums();
+  hoversImages();
 });
 
 function reloadAlbum(id)
@@ -16,7 +18,9 @@ function reloadAlbum(id)
       success: function(json){
           if(json.response == "OK")
           {
-            $("#album_images_"+id).html(json.options.body);
+            $("#album_images_container_"+id).html(json.options.body);
+            restartSlider(id);
+            hoversImages();
           }
       }, 
       complete: function()
@@ -24,4 +28,91 @@ function reloadAlbum(id)
 
       }
   });
+}
+
+var listOfAlbumSliders = new Array();
+
+function initializaseAllAlbums()
+{
+  //console.log(listOfAlbumSliders);
+  $(".album_images_container").each(function(index, item){
+    //console.log(index);
+    //console.log(item);
+    var currentId = $(item).attr('id');
+    var id = currentId.replace("album_images_container_","");
+    
+    startAlbumSlider(id);
+  });
+  //console.log(listOfAlbumSliders);
+}
+
+function restartSlider(id)
+{
+  listOfAlbumSliders[parseInt(id)].slider( "destroy" );
+  startAlbumSlider(id);
+}
+
+function startAlbumSlider(id)
+{
+  //console.log(id);
+  //vars
+  var conveyor = $("#album_images_container_"+id), item = $(".album_image", $("#album_images_container_"+id));
+  //console.log(conveyor);
+  //console.log(item);
+  //set length of conveyor
+  
+  conveyor.css("width", item.length * parseInt(item.css("width")));
+  //config
+  var myMax = parseInt((item.length * parseInt(item.css("width"))) - parseInt($("#view_album_images_"+id).css("width")));
+  
+  var sliderOpts = {
+    max: myMax,
+    slide: function(event, ui) {
+      conveyor.css("left", "-" + ui.value + "px");
+    }
+  };
+  //create slider
+  var scrollbar = $("#album_slider_"+id).slider(sliderOpts);
+  listOfAlbumSliders[parseInt(id)] = scrollbar;
+  return false;
+}
+
+function hoversImages()
+{
+  $('.album_image').each(function(index, value) {
+    $(this).hover(function(){
+      $(this).find('div.img_edit').show();
+      $(this).find('div.img_delete').show();
+    },
+    function(){
+      $(this).find('div.img_edit').hide();
+      $(this).find('div.img_delete').hide();
+    });
+  });
+  
+}
+
+function deleteFile(mUrl, text, itemId, albumId)
+{
+  if(confirm(text))
+  {
+    $.ajax({
+      url: mUrl,
+      data: {
+          'id': id
+      },
+      type: 'post',
+      dataType: 'json',
+      complete: function(json)
+      {
+        if(json.response == "OK")
+        {
+          $('#album_image_' + itemId).fadeOut(500, function() {$(this).remove();});
+          restartSlider(albumId);
+          hoversImages();
+        }
+      }        
+    });    
+  }
+
 }
