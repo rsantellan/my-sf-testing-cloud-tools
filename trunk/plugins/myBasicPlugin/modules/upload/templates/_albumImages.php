8f2cea0b39eddeb3b0bdda5e83ca4fbc7f263f29
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2012-04-25 20:04:04 +0000

    Se agrega el slider.

--- trunk/plugins/myBasicPlugin/modules/upload/templates/_albumImages.php
+++ trunk/plugins/myBasicPlugin/modules/upload/templates/_albumImages.php
@@ -1,7 +1,22 @@
 <?php
+    $firstImage = true;
     foreach($uploades as $upload):
       ?>
+<div id="album_image_<?php echo $upload->getId();?>" class="album_image">
+  <div class="img_edit">
+      <a href="<?php //echo site_url('upload/editFile/' . $image->id); ?>" class="fancy_link">
+        Editar
+      </a>
+    </div>
   <img src="<?php echo $upload->getUrl(array(myImageCodes::CODE => 1, myImageCodes::WIDTH => 200, myImageCodes::HEIGHT => 200));?>" />
+  <div class="img_delete">
+    <a onclick="return deleteFile('<?php //echo site_url('upload/deleteFile/'.$image->id);?>', <?php //echo $image->id;?>)" href="javascript:void(0)" class="">
+      <!-- <img src="<?php //echo base_url().'assets/upload/images/delete.png'?>" /> -->
+      <?php echo plugin_image_tag("myBasicPlugin", "trash.png", array('alt' => __('upload_delete'))); ?>
+    </a>
+  </div>
+</div>  
 <?php    
+    $firstImage = false;
   endforeach;
 ?>
--- trunk/plugins/myBasicPlugin/modules/upload/templates/_uploaderHeader.php
+++ trunk/plugins/myBasicPlugin/modules/upload/templates/_uploaderHeader.php
@@ -4,4 +4,7 @@
   use_plugin_javascript("myBasicPlugin", "fancybox/jquery.mousewheel-3.0.2.pack.js");
   use_plugin_stylesheet("myBasicPlugin", "../js/fancybox/jquery.fancybox-1.3.1.css");
   use_plugin_javascript("myBasicPlugin", "uploader.js", "last");
+  use_plugin_stylesheet("myBasicPlugin", "adminAlbum.css");
+  use_plugin_javascript("myBasicPlugin", "jquery-ui-1.8.16.custom.min.js");
+  use_plugin_stylesheet("myBasicPlugin", "le-frog/jquery-ui-1.8.16.custom.css");
 ?>
