a606703c2eec22cbce65ef2e28e1008108c167cc
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2012-05-04 22:49:50 +0000

    Se agrega el multi idioma como fijo para el backend.
    Se agrega las banderitas y se corrige el fixture.
    
    Lo que quedaria seria pasar el image magick por linea de comando.

--- trunk/apps/backend/lib/myUser.class.php
+++ trunk/apps/backend/lib/myUser.class.php
@@ -2,4 +2,12 @@
 
 class myUser extends sfGuardSecurityUser
 {
+  public function isFirstRequest($boolean = null)
+  {
+	if (is_null($boolean)) {
+	  return $this->getAttribute('first_request', true);
+	}
+
+	$this->setAttribute('first_request', $boolean);
+  }
 }
--- trunk/plugins/myBasicPlugin/web/js/layoutOptions.js
+++ trunk/plugins/myBasicPlugin/web/js/layoutOptions.js
@@ -4,3 +4,37 @@ $(document).ready(function() {
 
 });
 
+layoutOptions = function(options){
+  this._initialize();
+}
+
+layoutOptions.instance = null;
+
+layoutOptions.getInstance = function (){
+  if(layoutOptions.instance == null)
+	layoutOptions.instance = new layoutOptions();
+  return layoutOptions.instance;
+}
+
+layoutOptions.prototype = {
+  _initialize: function(){
+
+ },
+ 
+ clearCache: function(element)
+ {
+   //Supongo que estoy en un fancybox.
+   $.fancybox.showActivity();
+   $.ajax({
+	  url: $(element).attr("href"),
+	  dataType: 'json',
+	  complete: function(json){
+		//mdHideLoading();   
+		$.fancybox.hideActivity();
+	  }
+	});
+   
+   return false;
+ }
+ 
+}
