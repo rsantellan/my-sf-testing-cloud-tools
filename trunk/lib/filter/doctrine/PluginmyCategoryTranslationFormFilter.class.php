5546b04ff24faa37bcc197b93ed372bf77ea812c
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2013-02-07 19:56:41 +0000

    se crea el plugin de las categorias.

---
+++ trunk/lib/filter/doctrine/PluginmyCategoryTranslationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PluginmyCategoryTranslation filter form.
+ *
+ * @package    testing
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PluginmyCategoryTranslationFormFilter extends BasePluginmyCategoryTranslationFormFilter
+{
+  public function configure()
+  {
+  }
+}
