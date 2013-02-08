602346697083f38cf4916cb70f238494c147446f
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2013-02-08 19:20:01 +0000

    Se activa el plugin de music.

---
+++ lib/filter/doctrine/musicSheetsPlugin/base/BasemSheetFormFilter.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * mSheet filter form base class.
+ *
+ * @package    testing
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
+ */
+abstract class BasemSheetFormFilter extends BaseFormFilterDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'm_instrument_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('mInstrument'), 'add_empty' => true)),
+      'm_song_id'       => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('mSong'), 'add_empty' => true)),
+    ));
+
+    $this->setValidators(array(
+      'm_instrument_id' => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('mInstrument'), 'column' => 'id')),
+      'm_song_id'       => new sfValidatorDoctrineChoice(array('required' => false, 'model' => $this->getRelatedModelName('mSong'), 'column' => 'id')),
+    ));
+
+    $this->widgetSchema->setNameFormat('m_sheet_filters[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'mSheet';
+  }
+
+  public function getFields()
+  {
+    return array(
+      'id'              => 'Number',
+      'm_instrument_id' => 'ForeignKey',
+      'm_song_id'       => 'ForeignKey',
+    );
+  }
+}
