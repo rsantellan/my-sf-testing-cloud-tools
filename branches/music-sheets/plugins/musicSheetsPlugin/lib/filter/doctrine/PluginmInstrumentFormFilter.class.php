c8ab04b68b7cfdec0e178afb605357e10244e2ae
rsantellan@gmail.com <rsantellan@gmail.com@e43f80c6-ad6c-693d-3988-c76e21ff8788>
2013-02-08 19:18:02 +0000

    Se agregan la propiedad de abstracta a todas las clases.

--- branches/music-sheets/plugins/musicSheetsPlugin/lib/filter/doctrine/PluginmInstrumentFormFilter.class.php
+++ branches/music-sheets/plugins/musicSheetsPlugin/lib/filter/doctrine/PluginmInstrumentFormFilter.class.php
@@ -8,7 +8,7 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class PluginmInstrumentFormFilter extends BasemInstrumentFormFilter
+abstract class PluginmInstrumentFormFilter extends BasemInstrumentFormFilter
 {
   public function configure()
   {
--- branches/music-sheets/plugins/musicSheetsPlugin/lib/form/doctrine/PluginmSheetForm.class.php
+++ branches/music-sheets/plugins/musicSheetsPlugin/lib/form/doctrine/PluginmSheetForm.class.php
@@ -8,7 +8,7 @@
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class PluginmSheetForm extends BasemSheetForm
+abstract class PluginmSheetForm extends BasemSheetForm
 {
   public function setup()
   {
